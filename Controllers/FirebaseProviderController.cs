b5a21db3496f17ca0f7f68a3c14248efb406c734
thirukumarangceb <thirukumarangceb@gmail.com>
2019-10-07 17:13:00 +0530

    updated the firebase interface file name

--- Controllers/FirebaseProviderController.cs
+++ Controllers/FirebaseProviderController.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
 using Syncfusion.EJ2.FileManager.Base;
 using Syncfusion.EJ2.FileManager.FirebaseRealtimeFileProvider;
 
-namespace EJ2APIServices.Controllers
+namespace EJ2FirebaseRealtimeASPCoreFileProvider.Controllers
 {
     [Route("api/[controller]")]
     [EnableCors("AllowAllOrigins")]
@@ -22,7 +22,7 @@ namespace EJ2APIServices.Controllers
             this.operation.RegisterFirebaseRealtimeDB("https://filemanager-c0f6d.firebaseio.com/", "Files", hostingEnvironment.ContentRootPath + "\\FirebaseRealtimeDBHelper\\access_key.json");
         }
         [Route("FirebaseRealtimeFileOperations")]
-        public object FileOperations([FromBody] FileManagerDirectoryContent args)
+        public object FirebaseRealtimeFileOperations([FromBody] FileManagerDirectoryContent args)
         {
             if (args.Action == "delete" || args.Action == "rename")
             {
@@ -41,27 +41,36 @@ namespace EJ2APIServices.Controllers
             switch (args.Action)
             {
                 case "read":
+                    // reads the file(s) or folder(s) from the given path.
                     return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, false, args.Data));
                 case "delete":
+                    // deletes the selected file(s) or folder(s) from the given path.
                     return this.operation.ToCamelCase(this.operation.Delete(args.Path, args.Names, args.Data));
                 case "copy":
+                    // copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
                     return this.operation.ToCamelCase(this.operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
                 case "move":
+                    // cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
                     return this.operation.ToCamelCase(this.operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data));
                 case "details":
+                    // gets the details of the selected file(s) or folder(s).
                     return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
                 case "create":
+                    // creates a new folder in a given path.
                     return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name, args.Data));
                 case "search":
+                    // gets the list of file(s) or folder(s) from a given path based on the searched key string.
                     return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
                 case "rename":
+                    // renames a file or folder.
                     return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName, false, args.Data));
             }
             return null;
         }
 
+        // uploads the file(s) into a specified path
         [Route("FirebaseRealtimeUpload")]
-        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action, string data)
+        public IActionResult FirebaseRealtimeUpload(string path, IList<IFormFile> uploadFiles, string action, string data)
         {
             FileManagerResponse uploadResponse;
             FileManagerDirectoryContent[] dataObject = new FileManagerDirectoryContent[1];
@@ -77,15 +86,17 @@ namespace EJ2APIServices.Controllers
             return Content("");
         }
 
+        // downloads the selected file(s) and folder(s)
         [Route("FirebaseRealtimeDownload")]
-        public IActionResult Download(string downloadInput)
+        public IActionResult FirebaseRealtimeDownload(string downloadInput)
         {
             FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
             return operation.Download(args.Path, args.Names, args.Data);
         }
 
+        // gets the image(s) from the given path
         [Route("FirebaseRealtimeGetImage")]
-        public IActionResult GetImage(FileManagerDirectoryContent args)
+        public IActionResult FirebaseRealtimeGetImage(FileManagerDirectoryContent args)
         {
             return this.operation.GetImage(args.Path, args.Id, true, null, args.Data);
         }
